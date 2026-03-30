d0a14df558e09ff9e036256f086a5deca1f89f7e
Raghad-abdullah-444 <444052811@std.psau.edu.sa>
2026-03-30 15:35:26 +0300

    add int x

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -195,6 +195,7 @@ public class SchedulerSimulation {
         int studentID = 444052811;  // ← CHANGE THIS TO YOUR ACTUAL STUDENT ID//test
     
    //commit
+   int x=9;
         Random random = new Random(studentID);
         
         // Define the time quantum in milliseconds (the maximum time a process gets in one round)
