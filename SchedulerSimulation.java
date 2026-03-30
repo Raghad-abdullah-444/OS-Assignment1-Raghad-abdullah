68b11058847447f4398889bef5e759a57c3ceceb
Raghad-abdullah-444 <444052811@std.psau.edu.sa>
2026-03-30 15:43:11 +0300

    add note

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -193,7 +193,7 @@ public class SchedulerSimulation {
         // ⚠️ IMPORTANT: Put your student ID here to seed the random number generator
         // This makes your output unique to you - DO NOT forget to change this!
         int studentID = 444052811;  // ← CHANGE THIS TO YOUR ACTUAL STUDENT ID//test
-    
+    //note
 
   //commit
         Random random = new Random(studentID);
