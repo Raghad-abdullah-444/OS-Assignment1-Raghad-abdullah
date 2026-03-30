afd73b97ad759693726572bb16a5e496cdce3197
Raghad-abdullah-444 <444052811@std.psau.edu.sa>
2026-03-30 15:35:03 +0300

    add note

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -193,10 +193,8 @@ public class SchedulerSimulation {
         // ⚠️ IMPORTANT: Put your student ID here to seed the random number generator
         // This makes your output unique to you - DO NOT forget to change this!
         int studentID = 444052811;  // ← CHANGE THIS TO YOUR ACTUAL STUDENT ID//test
-    //test
-
-
-   
+    
+   //commit
         Random random = new Random(studentID);
         
         // Define the time quantum in milliseconds (the maximum time a process gets in one round)
